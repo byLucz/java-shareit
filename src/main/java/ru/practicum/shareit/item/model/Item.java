b28c85cf8b38972c1398c7c83c157f239469dac8
Dobronezsky <dobronezsky@ya.ru>
2024-07-20 19:57:17 +0700

    feat: added structure for base classes

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,28 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
+
+    private Integer id = null;
+
+    private String name;
+
+    private String description;
+
+    private Boolean available;
+
+    private User owner;
+
+    private ItemRequest request;
 }
---
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Integer id = null;
+
+    private String name;
+
+    private String email;
+}
