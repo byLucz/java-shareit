3dd8c7b90f3368a281af6c9fc70858c4070e103e
Dobronezsky <dobronezsky@ya.ru>
2024-07-20 20:10:06 +0700

    style: improved codestyle by actions check

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -6,9 +6,14 @@ import ru.practicum.shareit.user.dto.UserValidDto;
 import java.util.List;
 
 public interface UserService {
+
     UserValidDto createUser(UserValidDto userValidDto);
+
     UserValidDto getUser(Integer userId);
+
     UserValidDto updateUser(Integer userId, UserDto userDto);
+
     void deleteUser(Integer userId);
+
     List<UserValidDto> getAllUsers();
 }
