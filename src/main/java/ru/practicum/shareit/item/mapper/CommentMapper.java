8d7b6254ec9fa7a1b23dc844751ef40ae0acdf9b
Dobronezsky <dobronezsky@ya.ru>
2024-07-28 02:10:37 +0700

    feat: added comment component for items

---
+++ src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -0,0 +1,27 @@
+package ru.practicum.shareit.item.mapper;
+
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+public class CommentMapper {
+
+    public static CommentDto toCommentDto(Comment comment) {
+        CommentDto dto = new CommentDto();
+        dto.setId(comment.getId());
+        dto.setText(comment.getText());
+        dto.setAuthorName(comment.getUser().getName());
+        dto.setCreated(comment.getCreated());
+        return dto;
+    }
+
+    public static Comment toComment(CommentDto dto, User user, Item item) {
+        Comment comment = new Comment();
+        comment.setText(dto.getText());
+        comment.setUser(user);
+        comment.setItem(item);
+        comment.setCreated(dto.getCreated());
+        return comment;
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -0,0 +1,30 @@
+package ru.practicum.shareit.item.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 512)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime created;
+}
