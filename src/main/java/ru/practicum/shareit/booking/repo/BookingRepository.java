22a4a7f0b87b33390216a03266a43651e064b988
Dobronezsky <dobronezsky@ya.ru>
2024-08-08 05:08:53 +0700

    feat: added new query functions

--- src/main/java/ru/practicum/shareit/booking/repo/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repo/BookingRepository.java
@@ -2,48 +2,54 @@ package ru.practicum.shareit.booking.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingStatus;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Optional;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
-    List<Booking> findAllByBookerIdOrderByStartDateDesc(Integer bookerId);
-    List<Booking> findAllByItemOwnerIdOrderByStartDateDesc(Integer ownerId);
 
-    Optional<Booking> findFirst1ByItemIdAndBookerIdAndEndDateBefore(Integer itemId, Integer bookerId, LocalDateTime end);
-
-    @Query("SELECT b FROM Booking b WHERE b.item.id = :itemId AND b.startDate < CURRENT_TIMESTAMP ORDER BY b.startDate DESC")
+    @Query("SELECT b FROM Booking b WHERE b.item.id = :itemId AND b.start < CURRENT_TIMESTAMP ORDER BY b.start DESC")
     Optional<Booking> findLastBookingByItemId(Integer itemId);
 
-    @Query("SELECT b FROM Booking b WHERE b.item.id = :itemId AND b.startDate > CURRENT_TIMESTAMP ORDER BY b.startDate ASC")
+    @Query("SELECT b FROM Booking b WHERE b.item.id = :itemId AND b.start > CURRENT_TIMESTAMP ORDER BY b.start ASC")
     Optional<Booking> findNextBookingByItemId(Integer itemId);
 
-    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId AND b.endDate < :now ORDER BY b.startDate DESC")
-    List<Booking> findAllPastUserBookings(Integer userId, LocalDateTime now);
+    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId ORDER BY b.start DESC")
+    LinkedList<Booking> getAllUserBookings(@Param("userId") Integer userId);
 
-    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId AND b.startDate > :now ORDER BY b.startDate DESC")
-    List<Booking> findAllFutureUserBookings(Integer userId, LocalDateTime now);
+    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId AND b.start > :now ORDER BY b.start DESC")
+    LinkedList<Booking> getAllFutureUserBookings(@Param("userId") Integer userId, @Param("now") LocalDateTime now);
 
-    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId AND b.startDate <= :now AND b.endDate >= :now ORDER BY b.startDate DESC")
-    List<Booking> findAllCurrentUserBookings(Integer userId, LocalDateTime now);
+    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId AND b.status = :status ORDER BY b.start DESC")
+    LinkedList<Booking> getSpecialStateUserBookings(@Param("userId") Integer userId, @Param("status") BookingStatus status);
 
-    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId AND b.endDate < :now ORDER BY b.startDate DESC")
-    List<Booking> findPastUserItemBookings(Integer userId, LocalDateTime now);
+    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId AND b.end < :now ORDER BY b.start DESC")
+    LinkedList<Booking> getAllPastUserBookings(@Param("userId") Integer userId, @Param("now") LocalDateTime now);
 
-    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId AND b.startDate > :now ORDER BY b.startDate DESC")
-    List<Booking> findFutureUserItemBookings(Integer userId, LocalDateTime now);
+    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId AND b.start <= :now AND b.end >= :now ORDER BY b.start DESC")
+    LinkedList<Booking> getAllCurrentUserBookings(@Param("userId") Integer userId, @Param("now") LocalDateTime now);
 
-    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId AND b.startDate <= :now AND b.endDate >= :now ORDER BY b.startDate DESC")
-    List<Booking> findCurrentUserBookings(Integer userId, LocalDateTime now);
+    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId ORDER BY b.start DESC")
+    LinkedList<Booking> getAllUserItemBookings(@Param("userId") Integer userId);
 
-    @Query("SELECT b FROM Booking b WHERE b.booker.id = :userId AND b.status = :status ORDER BY b.startDate DESC")
-    List<Booking> findSpecialStateUserBookings(Integer userId, BookingStatus status);
+    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId AND b.start > :now ORDER BY b.start DESC")
+    LinkedList<Booking> getFutureUserItemBookings(@Param("userId") Integer userId, @Param("now") LocalDateTime now);
 
-    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId AND b.status = :status ORDER BY b.startDate DESC")
-    List<Booking> findSpecialStateUserItemBookings(Integer userId, BookingStatus status);
+    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId AND b.status = :status ORDER BY b.start DESC")
+    LinkedList<Booking> getSpecialStateUserItemBookings(@Param("userId") Integer userId, @Param("status") BookingStatus status);
+
+    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId AND b.end < :now ORDER BY b.start DESC")
+    LinkedList<Booking> getPastUserItemBookings(@Param("userId") Integer userId, @Param("now") LocalDateTime now);
+
+    @Query("SELECT b FROM Booking b WHERE b.item.owner.id = :userId AND b.start <= :now AND b.end >= :now ORDER BY b.start DESC")
+    LinkedList<Booking> getCurrentUserBookings(@Param("userId") Integer userId, @Param("now") LocalDateTime now);
+
+    @Query("SELECT b FROM Booking b WHERE b.item.id = :itemId AND b.booker.id = :userId AND b.end < :now ORDER BY b.end DESC")
+    Optional<Booking> findFirst1ByItemIdAndBookerIdAndEndIsBefore(@Param("itemId") Integer itemId, @Param("userId") Integer userId, @Param("now") LocalDateTime now);
 }
