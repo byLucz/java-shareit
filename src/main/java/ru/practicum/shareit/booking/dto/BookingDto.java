3bbc642c41a959419bd2bb3a737e5a915b66f4c1
dobronezsky <dobronezsky@ya.ru>
2024-08-08 15:12:45 +0700

    feat: extend bookindto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -3,6 +3,9 @@ package ru.practicum.shareit.booking.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +15,9 @@ import java.time.LocalDateTime;
 
 public class BookingDto {
     private Integer id;
-    private Integer bookerId;
     private LocalDateTime start;
     private LocalDateTime end;
+    private Item item;
+    private User booker;
+    private BookingStatus status;
 }
