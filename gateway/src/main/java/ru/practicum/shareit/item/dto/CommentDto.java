b7a704fd05d73f8a471f13cf70b03be44037dbfc
Dobronezsky <dobronezsky@ya.ru>
2024-08-11 06:17:33 +0700

    fix: replacing annotation

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,6 @@ public class CommentDto {
     private String text;
     @NotBlank(message = "Имя автора не может быть пустым")
     private String authorName;
-    @FutureOrPresent
+    @NotNull
     private LocalDateTime created;
 }
